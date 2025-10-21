ff2130477cd3a20dbb7089f5a025eaaaef6b5aa0
Brian Timothy Karani <timothykaranibrian@gmail.com>
2025-10-21 16:59:19 +0300

    fixed error basename error

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/hugs-frontend/", // 👈 Add this line
+  base: "/hugs-frontend/", // ensures assets and routing work on GitHub Pages
   plugins: [react(), tailwindcss()],
 });
